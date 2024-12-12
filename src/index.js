94e96bcd9f9485ea6c90ca71373e1e46a05650e4
chaimae01el <157977261+chaimae01el@users.noreply.github.com>
2024-12-12 23:27:31 +0100

    NetflixClone

---
+++ src/index.js
@@ -0,0 +1,32 @@
+document.getElementById("subscribe").addEventListener("click", function () {
+    const emailInput = document.querySelector(".email-input").value;
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(emailInput)) {
+        alert("Please enter a valid email address.");
+        return;
+    }
+
+    fetch("users.json")
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Error loading the JSON file.");
+            }
+            return response.json();
+        })
+        .then((data) => {
+            // email exists or not in json
+            const userExists = data.users.some(user => user.email === emailInput);
+
+            if (userExists) {
+                // Redirect to :
+                window.location.href = "home.html";
+            } else {
+                alert("Email not found in the database.");
+            }
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+            alert("Unable to verify the email at this time.");
+        });
+});
